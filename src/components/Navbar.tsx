7036cce91f7458e29c754f19803ab6c33c29112e
Ifty <iftyl1234@gmail.com>
2025-06-26 16:12:48 +0600

    sticky navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,42 +14,44 @@ const Navbar: React.FC = () => {
   }, [navigate]);
 
   return (
-    <div className={styles.navbar}>
-      <div className={styles.depth3Frame0}>
-        <img className={styles.depth4Frame0} alt="Logo" src="/Figma_photoes/wandernest.svg" />
-        <div className={styles.depth4Frame1} onClick={goHome}>
-          <b className={styles.wandernest}>WanderNest</b>
-        </div>
-      </div>
-
-      <div className={styles.depth3Frame1}>
-        <div className={styles.depth4Frame01}>
-          <div className={styles.depth4Frame1} onClick={() => navigate('/destinations')}>
-            <div className={styles.destinations}>Destinations</div>
-          </div>
-          <div className={styles.depth4Frame1} onClick={() => navigate('/hotels-rooms')}>
-            <div className={styles.destinations}>Hotels</div>
-          </div>
-          <div className={styles.depth5Frame2}>
-            <div className={styles.flights} onClick={goHome}>Flights</div>
-          </div>
-          <div className={styles.depth4Frame1} onClick={() => navigate('/Packages')}>
-            <div className={styles.destinations}>Packages</div>
+    <div className={styles.navbarWrapper}>
+      <div className={styles.navbar}>
+        <div className={styles.depth3Frame0}>
+          <img className={styles.depth4Frame0} alt="Logo" src="/Figma_photoes/wandernest.svg" />
+          <div className={styles.depth4Frame1} onClick={goHome}>
+            <b className={styles.wandernest}>WanderNest</b>
           </div>
         </div>
 
-        <div className={styles.depth4Frame11}>
-          <div className={styles.depth5Frame01} onClick={handleAuthClick}>
-            <div className={styles.depth6Frame0}>
-              <b className={styles.signUp}>Sign up</b>
+        <div className={styles.depth3Frame1}>
+          <div className={styles.depth4Frame01}>
+            <div className={styles.depth4Frame1} onClick={() => navigate('/destinations')}>
+              <div className={styles.destinations}>Destinations</div>
+            </div>
+            <div className={styles.depth4Frame1} onClick={() => navigate('/hotels-rooms')}>
+              <div className={styles.destinations}>Hotels</div>
+            </div>
+            <div className={styles.depth5Frame2}>
+              <div className={styles.flights} onClick={goHome}>Flights</div>
+            </div>
+            <div className={styles.depth4Frame1} onClick={() => navigate('/Packages')}>
+              <div className={styles.destinations}>Packages</div>
             </div>
           </div>
-          <div className={styles.depth5Frame11} onClick={handleAuthClick}>
-            <div className={styles.depth6Frame0}>
-              <b className={styles.signUp}>Log in</b>
+
+          <div className={styles.depth4Frame11}>
+            <div className={styles.depth5Frame01} onClick={handleAuthClick}>
+              <div className={styles.depth6Frame0}>
+                <b className={styles.signUp}>Sign up</b>
+              </div>
             </div>
+            <div className={styles.depth5Frame11} onClick={handleAuthClick}>
+              <div className={styles.depth6Frame0}>
+                <b className={styles.signUp}>Log in</b>
+              </div>
+            </div>
+            <img className={styles.depth5Frame21} alt="" src="/Figma_photoes/world.svg" />
           </div>
-          <img className={styles.depth5Frame21} alt="" src="/Figma_photoes/world.svg" />
         </div>
       </div>
     </div>
