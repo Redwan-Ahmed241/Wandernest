aa6adefecd017efc837bdec52c7deac8cf0e1ab8
Shah Redwan Ahmed <shah.redwan.ahmed.safin@gmail.com>
2025-08-12 21:31:34 +0600

    Fix react-datepicker source map loader warnings

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,26 @@
 module.exports = function override(config, env) {
-  // Add a new rule to exclude react-datepicker from source-map-loader
-  // This rule is added to the beginning of the rules array to ensure it takes precedence.
+  // Filter out existing source-map-loader rules to avoid conflicts
+  config.module.rules = config.module.rules.filter(rule => {
+    if (rule.use && Array.isArray(rule.use)) {
+      return !rule.use.some(loader => 
+        typeof loader === 'string' && loader.includes('source-map-loader')
+      );
+    }
+    if (rule.use && typeof rule.use === 'string') {
+      return !rule.use.includes('source-map-loader');
+    }
+    return true;
+  });
+
+  // Add a new source-map-loader rule that explicitly excludes react-datepicker
   config.module.rules.unshift({
-    test: /\.js$/,
+    test: /\.(js|mjs|jsx|ts|tsx)$/,
     enforce: 'pre',
     use: ['source-map-loader'],
-    exclude: /node_modules[\\/]react-datepicker/,
+    exclude: [
+      /node_modules[\\/]react-datepicker[\\/]/,
+      /node_modules[\\/]@.*[\\/]react-datepicker[\\/]/
+    ],
   });
   
   return config;
